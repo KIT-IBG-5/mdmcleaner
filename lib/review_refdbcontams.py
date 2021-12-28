26d31e0d26a42dafb55cd740ed6101899104a569
John Vollmers <john.vollmers@kit.edu>
2021-12-28 13:12:21 +0100

    added special case for silva/gtdb-taxonomy conflicts. changed 'count_contradictions' into a class method

--- lib/review_refdbcontams.py
+++ lib/review_refdbcontams.py
@@ -55,7 +55,6 @@ class comparison_hit(object):
 			self.blastdata = blasthandler.blastdata(*resultfiles, max_evalue = 1e-5, min_ident = 90, score_cutoff_fraction = 0, keep_max_hit_fraction = 1, keep_min_hit_count = 2, continue_from_json = False, auxilliary = False, seqtype=None, blacklist=blacklist) #todo: ignlorelistfile should be changed to ignore list. Can be a list of a filepath. If filepath, read that file as list. if list, use that
 			# ~ if self.seqid == "GCA_002842085.1_PHCA01000074.1": #todo:only for debugging
 				# ~ import pdb; pdb.set_trace()		
-			self.blastdata.add_info_to_blastlines(taxdb_obj=self.db)
 			# ~ delmeprint(self.blastdata.blastlinelist, self.db)
 			# ~ if self.seqid == "GCA_002842085.1_PHCA01000074.1": #todo:only for debugging
 				# ~ import pdb; pdb.set_trace()		
@@ -63,15 +62,114 @@ class comparison_hit(object):
 			if self.blast == "blastx":
 				self.blastdata.filter_blasthits_by_cov_and_ident(mincov=90, filterbylen=subject)
 			else:
-				self.blastdata.filter_blasthits_by_cov_and_ident()		
+				self.blastdata.filter_blasthits_by_cov_and_ident()	
+			self.blastdata.add_info_to_blastlines(taxdb_obj=self.db)	
 			# ~ print("afterfilter:")
 			# ~ delmeprint(self.blastdata.blastlinelist, self.db)
 			# ~ print("="*50)
 			# ~ if self.seqid == "GCA_002842085.1_PHCA01000074.1": #todo:only for debugging
 				# ~ import pdb; pdb.set_trace()			
-			return_category = count_contradictions(self.blastdata, self.db, self.domain, self.phylum, self.seqid)
+			return_category = self.count_contradictions()
 			print("*"*100)
 			return return_category
+
+	def count_contradictions(self): # argument "ch" should be a "comparison_hit"-object
+	#blastdata, db, comparison_domain, comparison_phylum, query_acc): #todo. add query-id to required arguments, so that hits to original query can be more safely recognized and ignored! 
+		import re
+		comparison_domain = re.sub("^d__","", self.domain)
+		comparison_phylum = re.sub("^p__","",self.phylum)
+		if comparison_domain == "None":
+			comparison_domain = None
+		if comparison_phylum == "None":
+			comparison_phylum = None
+		counts = {}
+		weights = {}
+		best_selfscore = 0
+		firstline = True
+		for line in self.blastdata.blastlinelist:
+			# ~ print(line)
+			# ~ print(firstline)
+			dp_tuple = self.db.get_domain_phylum(line["taxid"])
+			if firstline and line["subject"] == self.seqid:
+				best_selfscore = line["score"]
+				firstline = False
+			if dp_tuple in counts:
+				counts[dp_tuple] += 1
+				weights[dp_tuple] += line["score"]
+			else:
+				counts[dp_tuple] = 1
+				if line["subject"] == self.seqid:
+					weights[dp_tuple] = 0 #ignoring scores of hits to self
+				else: 
+					weights[dp_tuple] = line["score"]
+		print("expected: {}, {}".format(comparison_domain, comparison_phylum))
+		print("best_selfscore: {}".format(best_selfscore))
+		print("actual counts:\n\t{}".format("\n\t".join(["{} : {}".format(key, counts[key]) for key in counts.keys() ])))
+		print("actual weights:\n\t{}".format("\n\t".join(["{} : {}".format(key, weights[key]) for key in weights.keys() ])))
+		print("----------------------------")
+		# ~ if self.seqid == "GCA_002842085.1_PHCA01000074.1": #todo:only for debugging
+			# ~ import pdb; pdb.set_trace()
+		domain_counts = { domain: sum([counts[x] for x in counts.keys() if x[0] == domain]) for domain in set([y[0] for y in counts.keys()]) }
+		domain_weights = { domain: sum([weights[x] for x in weights.keys() if x[0] == domain]) for domain in set([y[0] for y in weights.keys()]) }
+		print("domain_counts:\n\t{}".format("\n\t".join(["{} : {}".format(key, domain_counts[key]) for key in domain_counts.keys() ])))
+		domain_counts_expected = domain_counts[comparison_domain]
+		domain_counts_contradicting = sum([domain_counts[x] for x in domain_counts.keys() if x != comparison_domain])
+		print("domain_counts expected == {}".format(domain_counts_expected))
+		print("domain_counts contradicting == {}".format(domain_counts_contradicting))
+		domain_weights_expected = domain_weights[comparison_domain]
+		domain_weights_contradicting = sum([domain_weights[x] for x in domain_weights.keys() if x != comparison_domain])
+		domain_weights_contradicting_excluding_none = sum([domain_weights[x] for x in domain_weights.keys() if (x != comparison_domain and x != None) ])
+		print("domain_weights:\n\t{}".format("\n\t".join(["{} : {}".format(key, domain_weights[key]) for key in domain_weights.keys() ])))
+		print("domain_weights expected == {}".format(domain_weights_expected))
+		print("domain_weights contradicting == {}".format(domain_weights_contradicting))
+		print("----------------------------")
+		phylum_counts = { phylum: sum([counts[x] for x in counts.keys() if x[1] == phylum]) for phylum in set([y[1] for y in counts.keys()]) }
+		phylum_weights= { phylum: sum([weights[x] for x in weights.keys() if x[1] == phylum]) for phylum in set([y[1] for y in weights.keys()]) }
+		print("phylum_counts:\n\t{}".format("\n\t".join(["{} : {}".format(key, phylum_counts[key]) for key in phylum_counts.keys() ])))
+		# ~ if self.seqid == "AF391990.1.1401":
+			# ~ import pdb; pdb.set_trace()
+		phylum_counts_expected = phylum_counts[comparison_phylum]
+		phylum_counts_contradicting = sum([phylum_counts[x] for x in phylum_counts.keys() if x != comparison_phylum])
+		print("phylum_counts expected == {}".format(phylum_counts_expected))
+		print("phylum_counts contradicting == {}".format(phylum_counts_contradicting))
+		phylum_weights_expected = phylum_weights[comparison_phylum]
+		phylum_weights_contradicting = sum([phylum_weights[x] for x in phylum_weights.keys() if x != comparison_phylum ])
+		phylum_weights_contradicting_excluding_none = sum([phylum_weights[x] for x in phylum_weights.keys() if (x != comparison_phylum and x != None)])
+		print("phylum_weights:\n\t{}".format("\n\t".join(["{} : {}".format(key, phylum_weights[key]) for key in phylum_weights.keys() ])))
+		print("phylum_weights expected == {}".format(phylum_weights_expected))
+		print("phylum_weights contradicting == {}".format(phylum_weights_contradicting))
+		print("phylum_weights contradicting (excluding 'None')== {}".format(phylum_weights_contradicting_excluding_none))
+		for pc in [domain_counts, phylum_counts]:
+			if len(pc) == 2 and None in pc.keys():
+				if "rRNA" in self.markerlevel:
+					print(" --> '{}' represents a contradiction between Silva and GTDB taxonomies! Ignoring Silva taxonomy for purpose of contamination detection in this case!".format(self.seqid))
+					return "silva_conflict"
+				else:
+					print(" --> there appear to be references similar to {} that are not annotated up to phylum level! This is likely an error in the reference DB!".format(self.seqid))
+					return "wtf"
+		# if contradicting counts larger than expectedhitcounts AND contradicting weights >= 2x expectedhitcounts: --> contamination
+		if domain_counts_contradicting > domain_counts_expected and domain_weights_contradicting >= (domain_weights_expected * 2) and domain_weights_contradicting >= (best_selfscore * 0.2): #the last requirement ensures that random matchtes of very small contigs representing e.g. partial transposases etc do not cause misclassification of very large contigs that match only in those short regions (in other words: very large contigs are only seen as contaminants if other large contigs match)
+			print(" ---> '{}' shows indication of being a contamination on domain level! --> ADD TO BLACKLIST!".format(self.seqid))
+			return "contamination"
+		# if contradcting counts >=  expectedhitcounts AND contradicting weights >= expectedhitwights: suspicious ambiguity! one may be contamination, but not sure which
+		if domain_counts_contradicting >= domain_counts_expected and domain_weights_contradicting >= domain_weights_expected and domain_weights_contradicting >= (best_selfscore * 0.1):
+			print(" '{}' has highly similar hits to genomes of other domains! either it or the matching contigs from other domains may be contaminations! --> requires independent evaluation (e.g. blast against refseq, uniprot or nr)".format(self.seqid))
+			return "ambiguity"
+		if phylum_counts_contradicting > phylum_counts_expected and phylum_weights_contradicting >= (phylum_weights_expected * 2) and phylum_weights_contradicting >= (best_selfscore * 0.2): #the last requirement ensures that random matchtes of very small contigs representing e.g. partial transposases etc do not cause misclassification of very large contigs that match only in those short regions (in other words: very large contigs are only seen as contaminants if other large contigs match)
+			print(" ---> '{}' shows indication of being a contamination on phylum level! --> ADD TO BLACKLIST!".format(self.seqid))
+			return "contamination"
+		# if contradcting counts >=  expectedhitcounts AND contradicting weights >= expectedhitwights: suspicious ambiguity! one may be contamination, but not sure which
+		if phylum_counts_contradicting >= phylum_counts_expected and phylum_weights_contradicting >= phylum_weights_expected and phylum_weights_contradicting >= (best_selfscore * 0.1):
+			print(" '{}' has highly similar hits to genomes of other phyla! either it or the matching contigs in other phyla may be contaminations! --> requires independent evaluation (e.g. blast against refseq, uniprot or nr)".format(self.seqid))
+			return "ambiguity"
+		# if contradicting counts < expectedhitcounts OR contrdicting weights < expectedhitweights: actually ok! --> drop ambiguity
+		if phylum_counts_contradicting < phylum_counts_expected or phylum_weights_contradicting < phylum_weights_expected or phylum_weights_contradicting < (best_selfscore * 0.1):
+			print("NO sufficient proof for '{}' being a contaminant. assuming it is OK!".format(self.seqid))
+			return "OK"
+		print("there is a case i have not considered, and THIS is it!")
+		return "wtf"
+		#todo: add detailed evidence strings to 'contamination' and 'ambiguity' classifications...
+		import pdb; pdb.set_trace()
 
 class comp_refseqprot(comparison_hit): #specifically for eukaryotic and viral references that are currently still only stored as protein sequences...
 	def __init__(self,*, taxid, seqid, domain, phylum, db, markerlevel, configs):
@@ -235,86 +333,6 @@ def read_ambiguity_report(ambiguity_report, configs, blacklist = None):
 			# ~ print("blastfiles:\n\t{}\n\t".format("\n\t".join(blastfiles))
 			# ~ blastdata = blasthandler.blastdata(blastfiles, max_evalue = 1e-5, min_ident = 90, score_cutoff_fraction = 0, keep_max_hit_fraction = 1, keep_min_hit_count = 2, continue_from_json = False, auxilliary = False, seqtype=None, ignorelistfile=None) #todo: ignlorelistfile should be changed to ignore list. Can be a list of a filepath. If filepath, read that file as list. if list, use that
 			# ~ import pdb; pdb.set_trace()
-
-def count_contradictions(blastdata, db, comparison_domain, comparison_phylum, query_acc): #todo. add query-id to required arguments, so that hits to original query can be more safely recognized and ignored! 
-	import re
-	comparison_domain = re.sub("^d__","",comparison_domain)
-	comparison_phylum = re.sub("^p__","",comparison_phylum)
-	counts = {}
-	weights = {}
-	best_selfscore = 0
-	firstline = True
-	for line in blastdata.blastlinelist:
-		# ~ print(line)
-		# ~ print(firstline)
-		dp_tuple = db.get_domain_phylum(line["taxid"])
-		if firstline and line["subject"] == query_acc:
-			best_selfscore = line["score"]
-			firstline = False
-		if dp_tuple in counts:
-			counts[dp_tuple] += 1
-			weights[dp_tuple] += line["score"]
-		else:
-			counts[dp_tuple] = 1
-			if line["subject"] == query_acc:
-				weights[dp_tuple] = 0 #ignoring scores of hits to self
-			else: 
-				weights[dp_tuple] = line["score"]
-	print("expected: {}, {}".format(comparison_domain, comparison_phylum))
-	print("best_selfscore: {}".format(best_selfscore))
-	print("actual counts:\n\t{}".format("\n\t".join(["{} : {}".format(key, counts[key]) for key in counts.keys() ])))
-	print("actual weights:\n\t{}".format("\n\t".join(["{} : {}".format(key, weights[key]) for key in weights.keys() ])))
-	print("----------------------------")
-	# ~ if query_acc == "GCA_002842085.1_PHCA01000074.1": #todo:only for debugging
-		# ~ import pdb; pdb.set_trace()
-	domain_counts = { domain: sum([counts[x] for x in counts.keys() if x[0] == domain]) for domain in set([y[0] for y in counts.keys()]) }
-	domain_weights = { domain: sum([weights[x] for x in weights.keys() if x[0] == domain]) for domain in set([y[0] for y in weights.keys()]) }
-	print("domain_counts:\n\t{}".format("\n\t".join(["{} : {}".format(key, domain_counts[key]) for key in domain_counts.keys() ])))
-	domain_counts_expected = domain_counts[comparison_domain]
-	domain_counts_contradicting = sum([domain_counts[x] for x in domain_counts.keys() if x != comparison_domain])
-	print("domain_counts expected == {}".format(domain_counts_expected))
-	print("domain_counts contradicting == {}".format(domain_counts_contradicting))
-	domain_weights_expected = domain_weights[comparison_domain]
-	domain_weights_contradicting = sum([domain_weights[x] for x in domain_weights.keys() if x != comparison_domain])
-	print("domain_weights:\n\t{}".format("\n\t".join(["{} : {}".format(key, domain_weights[key]) for key in domain_weights.keys() ])))
-	print("domain_weights expected == {}".format(domain_weights_expected))
-	print("domain_weights contradicting == {}".format(domain_weights_contradicting))
-	print("----------------------------")
-	phylum_counts = { phylum: sum([counts[x] for x in counts.keys() if x[1] == phylum]) for phylum in set([y[1] for y in counts.keys()]) }
-	phylum_weights= { phylum: sum([weights[x] for x in weights.keys() if x[1] == phylum]) for phylum in set([y[1] for y in weights.keys()]) }
-	print("phylum_counts:\n\t{}".format("\n\t".join(["{} : {}".format(key, phylum_counts[key]) for key in phylum_counts.keys() ])))
-	phylum_counts_expected = phylum_counts[comparison_phylum]
-	phylum_counts_contradicting = sum([phylum_counts[x] for x in phylum_counts.keys() if x != comparison_phylum])
-	print("phylum_counts expected == {}".format(phylum_counts_expected))
-	print("phylum_counts contradicting == {}".format(phylum_counts_contradicting))
-	phylum_weights_expected = phylum_weights[comparison_phylum]
-	phylum_weights_contradicting = sum([phylum_weights[x] for x in phylum_weights.keys() if x != comparison_phylum])
-	print("phylum_weights:\n\t{}".format("\n\t".join(["{} : {}".format(key, phylum_weights[key]) for key in phylum_weights.keys() ])))
-	print("phylum_weights expected == {}".format(phylum_weights_expected))
-	print("phylum_weights contradicting == {}".format(phylum_weights_contradicting))
-	# if contradicting counts larger than expectedhitcounts AND contradicting weights >= 2x expectedhitcounts: --> contamination
-	if domain_counts_contradicting > domain_counts_expected and domain_weights_contradicting >= (domain_weights_expected * 2) and domain_weights_contradicting >= (best_selfscore * 0.2): #the last requirement ensures that random matchtes of very small contigs representing e.g. partial transposases etc do not cause misclassification of very large contigs that match only in those short regions (in other words: very large contigs are only seen as contaminants if other large contigs match)
-		print(" ---> '{}' shows indication of being a contamination on domain level! --> ADD TO BLACKLIST!".format(query_acc))
-		return "contamination"
-	# if contradcting counts >=  expectedhitcounts AND contradicting weights >= expectedhitwights: suspicious ambiguity! one may be contamination, but not sure which
-	if domain_counts_contradicting >= domain_counts_expected and domain_weights_contradicting >= domain_weights_expected and domain_weights_contradicting >= (best_selfscore * 0.1):
-		print(" '{}' has hits to genomes of other domains! either it is a contamination, or the matching contigs in other domains are! --> evaluate independently (e.g. blast against refseq or uniprot)".format(query_acc))
-		return "ambiguity"
-	if phylum_counts_contradicting > phylum_counts_expected and phylum_weights_contradicting >= (phylum_weights_expected * 2) and phylum_weights_contradicting >= (best_selfscore * 0.2): #the last requirement ensures that random matchtes of very small contigs representing e.g. partial transposases etc do not cause misclassification of very large contigs that match only in those short regions (in other words: very large contigs are only seen as contaminants if other large contigs match)
-		print(" ---> '{}' shows indication of being a contamination on phylum level! --> ADD TO BLACKLIST!".format(query_acc))
-		return "contamination"
-	# if contradcting counts >=  expectedhitcounts AND contradicting weights >= expectedhitwights: suspicious ambiguity! one may be contamination, but not sure which
-	if phylum_counts_contradicting >= phylum_counts_expected and phylum_weights_contradicting >= phylum_weights_expected and phylum_weights_contradicting >= (best_selfscore * 0.1):
-		print(" '{}' has hits to genomes of other phyla! either it or the matching contigs in other phyla may be contaminations! --> evaluate independently (e.g. blast against refseq or uniprot)".format(query_acc))
-		return "ambiguity"
-	# if contradicting counts < expectedhitcounts OR contrdicting weights < expectedhitweights: actually ok! --> drop ambiguity
-	if phylum_counts_contradicting < phylum_counts_expected or phylum_weights_contradicting < phylum_weights_expected or phylum_weights_contradicting < (best_selfscore * 0.1):
-		print("NO sufficient proof for '{}' being a contaminant. assuming it is OK!".format(query_acc))
-		return "OK"
-	print("there is a case i have not considered, and THIS is it!")
-	#todo: add detailed evidence strings to 'contamination' and 'ambiguity' classifications...
-	import pdb; pdb.set_trace()
-
 
 # ~ def reevaluate_ambigeous_contigs():
 	# ~ '''
